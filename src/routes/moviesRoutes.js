20fc45e7a05dc2ba34589b6707f09788ea3ce03a
guiloeight <guiloeight@gmail.com>
2022-06-20 22:17:36 -0300

    up

--- src/routes/moviesRoutes.js
+++ src/routes/moviesRoutes.js
@@ -1,7 +1,10 @@
 const express = require("express");
+
 const router = express.Router();
 
 const controller = require("../controllers/moviesControllers");
 
+router.get("/all", controller.getAll);
+//routeando e se conectar ao controler
 
 module.exports = router;
